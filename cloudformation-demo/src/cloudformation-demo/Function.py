199e0d5964e653084c7b0dd7ad507593d1d7dc92
andrecarol <andrekonga5@yahoo.fr>
2024-06-23 15:23:12 +0200

    11eme

--- cloudformation-demo/src/cloudformation-demo/Function.py
+++ cloudformation-demo/src/cloudformation-demo/Function.py
@@ -1,14 +1,10 @@
 import json
 import logging
-import boto3
+from typing import Any, Dict
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from typing import Any, Dict
 
 logger = Logger(service="cloudformation_demo")
-
-# Initialize boto3 client for SQS if needed (e.g., to send messages)
-sqs = boto3.client('sqs')
 
 def lambda_handler(event: Dict[str, Any], context: LambdaContext) -> None:
     """
@@ -27,7 +23,4 @@ def process_message(message: Dict[str, Any], context: LambdaContext) -> None:
     # Simulate some processing work
     # TODO: Replace this with actual processing logic
     logger.info("Processing the message...")
-
-    # If you need to perform an async task, you can use asyncio or similar libraries
-    # For now, we'll just log that the task is done
     logger.info("Async task completed")
